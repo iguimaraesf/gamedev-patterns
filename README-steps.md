58979609dc60d49ff932c3e5423061f3ebbf81d2
Ivan Guimarães Farias <iguimaraesf@gmail.com>
2023-02-20 11:26:07 -0300

    feat: [Cap03] drawing the grid 1

--- README-steps.md
+++ README-steps.md
@@ -13,14 +13,18 @@ set pasta = "%~dp0%1"
 
 mkdir "%~dp0%1"
 
-docker run --rm --volume "%~dp0%1:/srv/react-docker" --workdir "/srv/react-docker" --publish 3000:3000 -it node:12.10.0 bash
+docker run --rm --volume "%~dp0%1:/srv/react-docker" --workdir "/srv/react-docker" --publish 8080:8080 -it node:12.10.0 bash
 ```
 # Chapter 02
 [Game Loop part 1/2](https://levelup.gitconnected.com/gamedev-patterns-and-algorithms-with-typescript-game-loop-part-1-2-699919bb9b71)
 
 Awesome! In this chapter, we learned what the Game Loop is and how we can combine it with Entity Component System.
 
-# Chapter 03
 [Game Loop part 2/2](https://levelup.gitconnected.com/gamedev-patterns-and-algorithms-in-action-with-typescript-game-loop-2-2-c0d57a8e5ec2)
 
 You should be proud of yourself, you have accomplished **A LOT**: learned about Game Loop and how it plays in one team with the Entity and Components, how to execute code every frame, discovered lifecycle methods of Entity and how to use them with the Game Loop, and, of course, build the core Game Entity and covered it with tests! Amazing progress!
+
+# Chapter 03
+[Drawing the grid 1/5](https://itnext.io/building-a-game-with-typescript-drawing-grid-1-5-aaf68797a0bb)
+
+Awesome! We have drawn our entire grid on the screen and make it look good. We also made it adjustable by the global settings, independently of code.
--- src/game/game.ts
+++ src/game/game.ts
@@ -1,3 +1,4 @@
+import { Settings } from '@/settings'
 import { Entity } from '@/utils'
 
 export class Game extends Entity {
@@ -18,6 +19,8 @@ export class Game extends Entity {
             // start update loop
             this.Update()
         })
+
+        this.DirtyDraw()
     }
 
     public Update(): void {
@@ -34,4 +37,26 @@ export class Game extends Entity {
         this._lastTimestamp = Date.now()
         window.requestAnimationFrame(() => this.Update())
     }
+
+    private DirtyDraw() : void {
+        // Create and attach Canvas to the DOM
+        const canvas = document.createElement('canvas')
+        const canvasSize = (Settings.grid.nodeSize + Settings.grid.nodeOffset) * Settings.grid.dimension + Settings.grid.nodeOffset
+
+        canvas.setAttribute('width', canvasSize.toString())
+        canvas.setAttribute('height', canvasSize.toString())
+        document.body.appendChild(canvas)
+    
+        const size = Settings.grid.nodeSize
+        const offset = Settings.grid.nodeOffset
+        for (let y = 0; y < Settings.grid.dimension; y++) {
+            for (let x = 0; x < Settings.grid.dimension; x++) {
+                const ctx = canvas.getContext('2d')!
+                ctx.beginPath()
+                ctx.fillStyle = Settings.grid.color
+                ctx.rect((size + offset) * x, (size + offset) * y, size, size)
+                ctx.fill()
+            }
+        }
+      }
 }
